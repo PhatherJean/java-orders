c8b9c990727200bb6d47789c45f0ff892b8c54cd
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-03-18 00:32:06 -0400

    redid day one

---
+++ src/main/java/com/lambdaschool/orders/models/Payment.java
@@ -0,0 +1,65 @@
+package com.lambdaschool.orders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "paments")
+public class Payment
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long paymentid;
+
+    @Column(nullable = false,
+    unique = true)
+    private String type;
+
+    @ManyToMany(mappedBy = "payments")
+    @JsonIgnoreProperties(value = "payments", allowSetters = true)
+    private Set<Order> orders = new HashSet<>();
+
+    public Payment()
+    {
+        //JPA use
+    }
+
+    public Payment(String type)
+    {
+        this.type = type;
+    }
+
+    public long getPaymentid()
+    {
+        return paymentid;
+    }
+
+    public void setPaymentid(long paymentid)
+    {
+        this.paymentid = paymentid;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public Set<Order> getOrders()
+    {
+        return orders;
+    }
+
+    public void setOrders(Set<Order> orders)
+    {
+        this.orders = orders;
+    }
+
+}
